b06460981ecb105bf3ee18f3396b1a05c5646106
Ацамаз В <aczamaz@gmail.com>
2023-02-01 13:14:39 +0300

    done constant

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,13 +1,33 @@
 import React from "react";
 import {StyleSheet,View,Text,Button} from 'react-native';
+import {THEME} from '../theme';
 
 export const TodoScreen = ({goBack,todo}) => {
     return (
         <View>
             <Text>{todo.title}</Text>
-            <Button title="Назад" onPress={goBack}/>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title="Назад" color={THEME.GREY_COLOR} onPress={goBack}/>
+                </View>   
+                <View style={styles.button}>
+                    <Button
+                        title="Удалить"
+                        color={THEME.DANGER_COLOR}
+                        onPress={()=>console.log('remove')}
+                    />
+                </View>   
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    buttons:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    button:{
+        width:'40%'
+    }
+});
